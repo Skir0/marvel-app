5c8efdae1051727045716cfe1537645b9b905995
Kirill Smychok <skiro6600200@gmail.com>
2023-08-03 12:11:22 +0300

    error handling and random character functionality

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,11 @@
+import img from "./error.gif"
+
+const ErrorMessage = () => {
+
+    return (
+        <img style={{display: "block", width: 250, height: 250, 
+            objectFit: "contain", margin: "0 auto"}} src={img} alt="Error"></img>
+    )
+}
+
+export default ErrorMessage;
